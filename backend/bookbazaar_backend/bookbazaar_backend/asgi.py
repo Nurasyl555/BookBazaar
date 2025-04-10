77e5dac67f860eefa7727d8d4dc1d34678f18d6d
Iam <kalmahanbet.nur06@gmail.com>
2025-04-10 09:36:43 +0500

    backend

--- backend/bookbazaar_backend/bookbazaar_backend/asgi.py
+++ backend/bookbazaar_backend/bookbazaar_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backendbook project.
+ASGI config for bookbazaar_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendbook.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookbazaar_backend.settings')
 
 application = get_asgi_application()
--- backend/bookbazaar_backend/manage.py
+++ backend/bookbazaar_backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendbook.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookbazaar_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
